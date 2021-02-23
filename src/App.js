49ca3482c7f5cfd6682feb265e3e222443a7ee85
Ariel Leguizamon <a.leguizamonariel@gmail.com>
2021-02-23 10:29:41 -0300

    readme + build folder

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import "./App.css";
 import "@shopify/polaris/dist/styles.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { AppProvider, Page } from "@shopify/polaris";
 import TransactionCard from "./components/transactioncard";
+import { API_URL } from "./configs.js";
 
 function App() {
-  const a = [
-    {
-      id: 0,
-      type: "credit",
-      amount: 100,
-      effectiveDate: 1614011841291,
-    },
-    {
-      id: 0,
-      type: "debit",
-      amount: 20,
-      effectiveDate: 1614011841291,
-    },
-    {
-      id: 0,
-      type: "credit",
-      amount: 100,
-      effectiveDate: 1614011841291,
-    },
-    {
-      id: 0,
-      type: "credit",
-      amount: 100,
-      effectiveDate: 1614011841291,
-    },
-    {
-      id: 0,
-      type: "debit",
-      amount: 30,
-      effectiveDate: 1614011841291,
-    },
-  ];
+  const [transactions, setTransactions] = useState([]);
+  const [error, setError] = useState(false);
+
+  const fetchTransactions = async () => {
+    try {
+      const resp = await axios.get(`${API_URL}/transactions`);
+      setTransactions(resp.data);
+    } catch (err) {
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
 
   return (
     <AppProvider>
       <Page>
         <div style={{ height: "200px" }}>
-          {a.map((e) => (
-            <TransactionCard transaction={e} />
+          {error && <p>There was an error fetching the data</p>}
+          {transactions.map((e) => (
+            <TransactionCard transaction={e} key={e.id} />
           ))}
         </div>
       </Page>
--- src/components/transactioncard.js
+++ src/components/transactioncard.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 import {
   Stack,
-  Link,
   Collapsible,
   Card,
   Button,
